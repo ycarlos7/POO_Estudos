ed90a4109288a40adcf4b4678932afc6c92e78bc
Yan Carlos <ycarlospereira@gmail.com>
2025-08-18 17:21:18 -0300

    Aula de revisão

---
+++ Revisao/Program.cs
@@ -0,0 +1,51 @@
+﻿//estruturas de repetição
+int i = 1;
+do
+{
+    Console.WriteLine("5 x " + i + " = " + 5 * i);
+    i = i + 1;
+}while(i <= 10);
+/*
+while (i <= 10)
+{
+    Console.WriteLine("5 x " + i + " = " + 5 * i);
+    i = i + 1;
+}
+for (int i = 1; i <= 10; i ++)
+    Console.WriteLine("5 x " + i + " = " + 5 * i);
+
+int i = 1;
+Console.WriteLine("valor de i" + i);
+*/
+//estrutura de decisão
+/*Console.WriteLine("Menu de cálculos");
+Console.WriteLine("1 - somar");
+Console.WriteLine("2 - subtrair");
+int opcao = 3;
+switch (opcao)
+{
+    case 1: Console.WriteLine("Realizando soma");
+            break;
+    case 2:
+        Console.WriteLine("Realizando subtração");
+        break;
+    default:
+        Console.WriteLine("Opção inválida");
+        break;
+}*/
+
+/*
+int idade = 0; //not - não - !
+if (idade >= 18) // or - ou - ||
+    Console.WriteLine("Maior idade");
+else if (idade == 0) //and - e - &&
+    Console.WriteLine("Não nasceu");
+else//opcional
+{
+    Console.WriteLine("Menor idade");
+}*/
+
+/*Console.Write("Digite o ano nascimento: ");
+int ano = Convert.ToInt32(Console.ReadLine());
+int idade = 2025 - ano;
+Console.WriteLine("Sua idade é " + idade);*/ 
